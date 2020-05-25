2c50ffe51c27e583496db05bc290eeeddd98f24d
Brittany Isenberg <brittany.isenberg@gmail.com>
2020-05-25 19:34:20 -0400

    styling

--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,6 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import {
-  FaReact,
-  FaPython,
-  FaWordpressSimple,
   FaUserCircle,
   FaPaperPlane,
   FaLinkedinIn,
@@ -18,50 +15,30 @@ const Header = ({ siteTitle }) => (
       <h4 class="title__sub">Web Developer & Designer</h4>
     </div>
     <nav class="links">
-      <Link
-        class="links__a hidden-large"
-        to="#react"
-        aria-label="React Project"
-      >
-        <FaReact />
-      </Link>
-      <Link
-        class="links__a hidden-large"
-        to="#django"
-        aria-label="Django Project"
-      >
-        <FaPython />
-      </Link>
-      <Link
-        class="links__a hidden-large"
-        to="#wordpress"
-        aria-label="WordPress Project"
-      >
-        <FaWordpressSimple />
-      </Link>
-      <hr class="links__hr hidden-large" />
       <Link class="links__a" to="#about" aria-label="About">
         <FaUserCircle />
       </Link>
       <Link class="links__a" to="#contact" aria-label="Contact">
         <FaPaperPlane />
       </Link>
-      <Link
+      <a
         class="links__a"
         to="https://www.linkedin.com/in/brittanyisenberg/"
         target="_blank"
+        rel="noopener noreferrer"
         aria-label="LinkedIn Profile"
       >
         <FaLinkedinIn />
-      </Link>
-      <Link
+      </a>
+      <a
         class="links__a"
         to="https://github.com/brittanyleigh"
         target="_blank"
+        rel="noopener noreferrer"
         aria-label="GitHub Profile"
       >
         <FaGithub />
-      </Link>
+      </a>
     </nav>
   </header>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,94 +6,103 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 import data from "../../content/homepage.json"
+import { FaCode, FaExternalLinkAlt } from "react-icons/fa"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div class="grid grid--thirds">
-      <div class="project" id="react">
-        <Image className="project__img" image="baseball-react.jpg" />
-        <h3 class="project__heading">MLB Team Stats App</h3>
-        <p class="project__description flex">
-          React & Redux web app for tracking your favorite MLB team's game
-          details, standings, player stats, and recent news.
-        </p>
-        <div class="project__buttons">
+    <div className="grid grid--thirds grid--50">
+      <div className="project grid" id="react">
+        <div className="project__img project--bottom-auto">
+          <Image filename="baseball-react.jpg" alt="react baseball project" />
+        </div>
+        <div className="project--bottom-auto">
+          <h3 className="project__heading">MLB Team Stats App</h3>
+          <p className="project__description flex">
+            React & Redux web app for tracking your favorite MLB team's game
+            details, standings, player stats, and recent news.
+          </p>
+        </div>
+        <div className="project__buttons">
           <a
-            class="button"
+            className="button"
             href="https://github.com/brittanyleigh/baseball-react"
             target="_blank"
-            rel="nofollow noreferrer"
+            rel="noopener noreferrer"
           >
-            <i class="fas fa-code"></i> View Code
+            <FaCode /> View Code
           </a>
           <a
-            class="button"
+            className="button"
             href="http://baseball.brittanyisenberg.com/"
             target="_blank"
-            rel="nofollow noreferrer"
+            rel="noopener noreferrer"
           >
-            <i class="fas fa-external-link-alt"></i> View Live
+            <FaExternalLinkAlt /> View Live
           </a>
         </div>
       </div>
-      <div class="project" id="django">
-        <img
-          class="project__img"
-          src="../images/habits.jpg"
-          alt="screenshot of habits app homepage"
-        />
-        <h3 class="project__heading">Essentials Habit Tracker</h3>
-        <p class="project__description flex">
-          Django web app for developing healthy, positive habits by tracking
-          daily streaks for three personalized 'essentials'.
-        </p>
-        <div class="project__buttons">
+      <div className="project grid" id="django">
+        <div className="project__img project--bottom-auto">
+          <Image filename="habits.jpg" alt="django habits" />
+        </div>
+        <div>
+          <h3 className="project__heading">Essentials Habit Tracker</h3>
+          <p className="project__description flex">
+            Django web app for developing healthy, positive habits by tracking
+            daily streaks for three personalized 'essentials'.
+          </p>
+        </div>
+        <div className="project__buttons">
           <a
-            class="button"
+            className="button"
             href="https://github.com/brittanyleigh/habits"
             target="_blank"
-            rel="noreferrer nofollow"
+            rel="noopener noreferrer"
           >
-            <i class="fas fa-code"></i> View Code
+            <FaCode /> View Code
           </a>
           <a
-            class="button"
+            className="button"
             href="http://brittanyleigh.pythonanywhere.com/"
             target="_blank"
-            rel="noreferrer nofollow"
+            rel="noopener noreferrer"
           >
-            <i class="fas fa-external-link-alt"></i> View Live
+            <FaExternalLinkAlt /> View Live
           </a>
         </div>
       </div>
-      <div class="project" id="wordpress">
-        <img
-          class="project__img"
-          src="../images/ryp.jpg"
-          alt="screenshot of Rochester Young Professionals website homepage"
-        />
-        <h3 class="project__heading">Rochester Young Professionals</h3>
-        <p class="project__description flex">
-          Custom WordPress theme design and development for non-profit
-          organization.
-        </p>
-        <div class="project__buttons">
+      <div className="project grid" id="wordpress">
+        <div className="project__img project--bottom-auto">
+          <Image
+            className="project__img"
+            filename="ryp.jpg"
+            alt="WordPress RYP site"
+          />
+        </div>
+        <div>
+          <h3 className="project__heading">Rochester Young Professionals</h3>
+          <p className="project__description flex">
+            Custom WordPress theme design and development for non-profit
+            organization.
+          </p>
+        </div>
+        <div className="project__buttons">
           <a
-            class="button"
+            className="button"
             href="https://github.com/brittanyleigh/ryp"
             target="_blank"
-            rel="noreferrer nofollow"
+            rel="noopener noreferrer"
           >
-            <i class="fas fa-code"></i> View Code
+            <FaCode /> View Code
           </a>
           <a
-            class="button"
+            className="button"
             href="http://r-y-p.org/"
             target="_blank"
-            rel="noreferrer nofollow"
+            rel="noopener noreferrer"
           >
-            <i class="fas fa-external-link-alt"></i> View Live
+            <FaExternalLinkAlt /> View Live
           </a>
         </div>
       </div>
