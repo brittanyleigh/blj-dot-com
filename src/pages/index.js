758b7b82bc6a88fc39019e07c2e13bcef771bf25
Brittany Isenberg <brittany.isenberg@gmail.com>
2020-05-25 20:40:00 -0400

    try deleting content

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,26 +2,11 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Project from "../components/project"
-import data from "../../content/homepage.json"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div className="grid grid--thirds grid--50">
-      {data.projects.map(item => {
-        return (
-          <Project
-            key={item.filename}
-            filename={item.filename}
-            alt={item.alt}
-            title={item.title}
-            description={item.description}
-            code={item.code}
-            live={item.live}
-          ></Project>
-        )
-      })}
-    </div>
+    <div className="grid grid--thirds grid--50"></div>
   </Layout>
 )
 
