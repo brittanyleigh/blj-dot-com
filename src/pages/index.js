b737ab8fa1545a79a60932dfe176d0202746a2e4
Brittany Isenberg <brittany.isenberg@gmail.com>
2020-05-29 16:33:43 -0400

    add projects back

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,26 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Project from "../components/project"
+import data from "../../content/homepage.json"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div className="grid grid--thirds grid--50"></div>
+    <div className="grid grid--thirds grid--50">
+      {data.projects.map(item => {
+        return (
+          <Project
+            key={item.image}
+            image={item.image}
+            alt={item.alt}
+            title={item.title}
+            description={item.description}
+            code={item.code}
+            live={item.live}
+          ></Project>
+        )
+      })}
+    </div>
   </Layout>
 )
 
