d4ccae435112f8e2215666d148781c6d58a9ee65
Brittany Isenberg <brittany.isenberg@gmail.com>
2020-08-30 13:49:51 -0400

    start setting up about and reviews

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,15 +2,20 @@ import React from "react"
 import PropTypes from "prop-types"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import PageHeading from "../components/pageHeading"
+import data from "../../content/about.json"
 
-const AboutPage = props => (
-  <Layout location={props.location}>
-    <SEO title="Home" />
-    <div className="grid grid--thirds grid--50">
-      <p>Abouts</p>
-    </div>
-  </Layout>
-)
+const AboutPage = props => {
+  return (
+    <Layout location={props.location}>
+      <SEO title="Home" />
+      <div className="m-8 sm:m-auto max-w-xl sm:my-16">
+        <PageHeading heading={data.heading} subheading={data.subheading} />
+        <div className="py-6">{data.about}</div>
+      </div>
+    </Layout>
+  )
+}
 
 AboutPage.propTypes = {
   location: PropTypes.object.isRequired,
--- src/pages/reviews.js
+++ src/pages/reviews.js
@@ -2,18 +2,23 @@ import React from "react"
 import PropTypes from "prop-types"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import PageHeading from "../components/pageHeading"
+import data from "../../content/reviews.json"
 
-const ReviewPage = props => (
-  <Layout location={props.location}>
-    <SEO title="Home" />
-    <div className="grid grid--thirds grid--50">
-      <p>Reviews</p>
-    </div>
-  </Layout>
-)
+const ReviewsPage = props => {
+  return (
+    <Layout location={props.location}>
+      <SEO title="Home" />
+      <div className="m-8 sm:m-auto max-w-xl sm:my-16">
+        <PageHeading heading={data.heading} subheading={data.subheading} />
+        <div className="grid grid-reviews">{data.about}</div>
+      </div>
+    </Layout>
+  )
+}
 
-ReviewPage.propTypes = {
+ReviewsPage.propTypes = {
   location: PropTypes.object.isRequired,
 }
 
-export default ReviewPage
+export default ReviewsPage
